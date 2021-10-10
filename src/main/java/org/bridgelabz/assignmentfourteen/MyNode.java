8610cd24071d724bd5a396743aa2f5d817058b4c
Sam-19091994 <jadhavdevendra40@gmail.com>
2021-10-10 19:15:49 +0530

    Creating A Simple Linked List With Key Values 56 30 70

---
+++ src/main/java/org/bridgelabz/assignmentfourteen/MyNode.java
@@ -0,0 +1,25 @@
+package org.bridgelabz.assignmentfourteen;
+
+
+public class MyNode<K> {
+    private K key;
+    private MyNode next;
+
+    public MyNode(K key) {
+        this.key = null;
+        this.next = null;
+    }
+
+    public MyNode getNext() {
+        return next;
+    }
+
+    public void setNext(MyNode next) {
+        this.next = next;
+    }
+
+    public static void main(String[] args )
+    {
+
+    }
+}
