8567c6aadc35beb53fa8fab35a0ec0bc92508005
Sam-19091994 <jadhavdevendra40@gmail.com>
2021-10-10 22:18:45 +0530

    Making An Append Function To Append The Keys In A Linked List

--- src/test/java/org/bridgelabz/assignmentfourteen/MyLinkedListTest.java
+++ src/test/java/org/bridgelabz/assignmentfourteen/MyLinkedListTest.java
@@ -8,30 +8,48 @@ import org.junit.Test;
 import java.util.LinkedList;
 
 public class MyLinkedListTest {
-    public static final Logger LOG = LogManager.getLogger(MyLinkedListTest.class);
+
     @Test
-    public void given3NumbersWhenAddedToLinkedListShouldBeAddedToTop(){
-        MyNode<Integer> myFirstNode = new MyNode<>(70 );
-        MyNode<Integer> mySecondNode = new MyNode<>(30 );
-        MyNode<Integer> myThirdNode = new MyNode<>(56 );
+    public void given3NumbersWhenAddedToLinkedListShouldBeAddedToTop() {
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
         MyLinkedList myLinkedList = new MyLinkedList();
         myLinkedList.add(myFirstNode);
         myLinkedList.add(mySecondNode);
         myLinkedList.add(myThirdNode);
 
         boolean result = myLinkedList.head.equals(myThirdNode) &&
-                         myLinkedList.head.getNext().equals(mySecondNode) &&
-                         myLinkedList.tail.equals(myFirstNode);
+                myLinkedList.head.getNext().equals(mySecondNode) &&
+                myLinkedList.tail.equals(myFirstNode);
         Assert.assertTrue(result);
+    }
 
-        LOG.debug("Debugging");
-        LOG.info("Info");
-        LOG.warn("Warning");
-        LOG.error("Error");
-        LOG.fatal("Fatal");
-        myLinkedList.printMyNodes();
+    public static final Logger LOG = LogManager.getLogger(MyLinkedListTest.class);
+        @Test
+        public void given3NumbersWhenAppendedShouldBeAddedToLast(){
+            MyNode<Integer> myFirstNode = new MyNode<>(56 );
+            MyNode<Integer> mySecondNode = new MyNode<>(30 );
+            MyNode<Integer> myThirdNode = new MyNode<>(70 );
+            MyLinkedList myLinkedList = new MyLinkedList();
+            myLinkedList.add(myFirstNode);
+            myLinkedList.append(mySecondNode);
+            myLinkedList.append(myThirdNode);
+            boolean result = myLinkedList.head.equals(myFirstNode) &&
+                             myLinkedList.head.getNext().equals(mySecondNode) &&
+                             myLinkedList.tail.equals(myThirdNode);
+
+            LOG.debug("Debugging");
+            LOG.info("Info");
+            LOG.warn("Warning");
+            LOG.error("Error");
+            LOG.fatal("Fatal");
+            myLinkedList.printMyNodes();
+        }
+
+
 
 
 
     }
-}
+
